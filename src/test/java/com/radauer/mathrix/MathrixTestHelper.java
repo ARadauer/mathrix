74006e96133125926334039e82ad7f9f0477f720
Andreas Radauer <andreas.radauer@porscheinformatik.at>
2018-01-05 14:27:00 +0100

    formating

--- src/test/java/com/radauer/mathrix/MathrixTestHelper.java
+++ src/test/java/com/radauer/mathrix/MathrixTestHelper.java
@@ -8,18 +8,21 @@ import java.math.BigDecimal;
 /**
  * Utitility Methos for helping test the Mathrix
  */
-public class MathrixTestHelper {
+public class MathrixTestHelper
+{
 
     private static RowTypeFactory rowTypeFactory = new RowTypeFactoryImpl();
     private static GroupKeyFactory groupKeyFactory = new GroupKeyFactoryImpl();
 
-    public static Position createPosition(String groupCode, String rowKeyTypeCode, String value) {
+    public static Position createPosition(String groupCode, String rowKeyTypeCode, String value)
+    {
         RowType rowType = rowTypeFactory.getRowTyp(rowKeyTypeCode);
         RowKey rowKey = new RowKey(rowType, "");
         return new Position(getGroupKey(groupCode), rowKey, new BigDecimal(value));
     }
 
-    public static void testValue(String groupCode, String rowKeyTypeCode, String valueString, Mathrix mat) {
+    public static void testValue(String groupCode, String rowKeyTypeCode, String valueString, Mathrix mat)
+    {
         RowType rowType = getRowType(rowKeyTypeCode);
         RowKey rowKey = new RowKey(rowType, "");
         Position pos = mat.getPosition(getGroupKey(groupCode), rowKey);
@@ -27,30 +30,37 @@ public class MathrixTestHelper {
         assertTrue(pos.getValue().compareTo(new BigDecimal(valueString)) == 0);
     }
 
-    public static RowType getRowType(String rowKeyTypeCode) {
+    public static RowType getRowType(String rowKeyTypeCode)
+    {
         return rowTypeFactory.getRowTyp(rowKeyTypeCode);
     }
 
-    public static RowKey getRowKey(String rowKeyTypeCode) {
+    public static RowKey getRowKey(String rowKeyTypeCode)
+    {
         return new RowKey(getRowType(rowKeyTypeCode), "");
     }
 
-    public static RowType[] getRowTypes(String... typeCodes) {
+    public static RowType[] getRowTypes(String... typeCodes)
+    {
         RowType[] result = new RowType[typeCodes.length];
-        for (int i = 0; i < typeCodes.length; i++) {
+        for (int i = 0; i < typeCodes.length; i++)
+        {
             result[i] = getRowType(typeCodes[i]);
         }
         return result;
 
     }
 
-    public static GroupKey getGroupKey(String groupKeyCode) {
+    public static GroupKey getGroupKey(String groupKeyCode)
+    {
         return groupKeyFactory.getGroupKey(groupKeyCode);
     }
 
-    public static GroupKey[] getGroupKeys(String... groupKeyCodes) {
+    public static GroupKey[] getGroupKeys(String... groupKeyCodes)
+    {
         GroupKey[] result = new GroupKey[groupKeyCodes.length];
-        for (int i = 0; i < groupKeyCodes.length; i++) {
+        for (int i = 0; i < groupKeyCodes.length; i++)
+        {
             result[i] = getGroupKey(groupKeyCodes[i]);
         }
         return result;
